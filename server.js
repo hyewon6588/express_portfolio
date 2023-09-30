dba524d4eff69fb5723630c9237403c664f74f76
hyewon ham <hyewon6588@naver.com>
2023-09-30 10:30:16 -0400

    error - server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ Date : 2023.09.30
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
-app.listen(3000);
+// app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+
 module.exports = app;
 
 console.log('Server running at http://localhost:3000/');
